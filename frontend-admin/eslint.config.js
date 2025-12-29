536118a6c1c5b07f02b4dad2ce5cb5197eafe681
Mirnes Mrso <mimr24@student.bth.se>
2025-12-29 21:56:49 +0100

    Frontend-admin: adjust ESLint rules (warnings instead of errors)

--- frontend-admin/eslint.config.js
+++ frontend-admin/eslint.config.js
@@ -23,7 +23,10 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+      "react-hooks/set-state-in-effect": "off",
+      "react-refresh/only-export-components": "off",
+      "no-unused-vars": "warn"
+    }
+
   },
 ])

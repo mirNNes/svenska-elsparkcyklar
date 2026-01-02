5aeaa76204703e16bc2e477c5ba7df7d5402278e
beckabeckis <rebeckacorell@gmail.com>
2026-01-02 18:08:57 +0100

    Fixed renting bikes- lint fixes 2

--- frontend-customer-app/src/pages/Dashboard.jsx
+++ frontend-customer-app/src/pages/Dashboard.jsx
@@ -7,21 +7,15 @@ export default function Dashboard() {
   const [mapKey] = useState(0);
   const navigate = useNavigate();
   
-  let activeRide = false;
-
   useEffect(() => {
     (async () => {
       try {
         const res = await getActiveRide();
         console.log(res);
         if (res.ride != null) {
-          activeRide = true;
-        }
-        if(activeRide) {
           navigate("/current_ride", { replace: true });
         }
       } catch (err) {
-        activeRide = false;
         console.log(err);
       }
     })();
